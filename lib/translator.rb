d1986e504c18250b1752a48265d9098dc2e12fe2
Terranie Sims <terranie67@gmail.com>
2020-09-22 18:50:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,36 +1,34 @@
 require 'yaml'
 require 'pry'
 
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  emoticon_hash = Hash.new
 
-def load_library(emoticon_file)
-  emoticons = YAML.load_file('./lib/emoticons.yml')
+  emoticon_hash["get_emoticon"] = Hash.new
+  emoticon_hash["get_meaning"] = Hash.new
 
-  emoticon_lib = {
-                  'get_meaning'  => { :english => {}},
-                  'get_emoticon' => { :japanese => {}} 
-    
-  }
-
-  emoticons.each do |meaning, value|  
-    # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
-    english = value[0]
-    japanese = value[1]
-    emoticon_lib['get_meaning'][japanese] = meaning
-    emoticon_lib['get_emoticon'][english] = japanese
+  emoticons.each do |english_word, emoticon_set|
+    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
+    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
   end
-  emoticon_lib
+  emoticon_hash
 end
 
+def get_japanese_emoticon(path, emoticon)
+  emoticon_hash = load_library(path)
+  result = emoticon_hash["get_emoticon"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found" 
+  end
+  result
+end
 
-def get_japanese_emoticon(emoticon_file, emoticon)
-  emoticon_lib = load_library(emoticon_file)
-  japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
-  japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
-end	
-
-
-def get_english_meaning(emoticon_file, emoticon)
-  # code goes here	  emoticon_lib = load_library(emoticon_file)
-  english_meaning = emoticon_lib['get_meaning'][emoticon]
-  english_meaning ? english_meaning : 'Sorry, that emoticon was not found'
-end	
+def get_english_meaning(path, emoticon)
+  emoticon_hash = load_library(path)
+  result = emoticon_hash["get_meaning"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found" 
+  end
+  result
+end
