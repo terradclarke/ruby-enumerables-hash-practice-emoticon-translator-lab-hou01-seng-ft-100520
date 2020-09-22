da8171e757f11c7394878e8ee911880b9abf5055
Terranie Sims <terranie67@gmail.com>
2020-09-22 19:07:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,15 +1,34 @@
 require 'yaml'
 require 'pry'
 
-def load_library(file_path)
- #code goes here
-  emo = YAML.load_file("lib/emoticons.yml")
-  frank_emo = {}
-  emo.each do |name,arrays|
-      frank_emo[name] ||= {}
-      frank_emo[name][:english] = arrays[0]
-      frank_emo[name][:japanese] = arrays[1]
+def load_library(path)
+
+  emoticons = YAML.load_file("lib/emoticons.yml")
+  frank_emoticons = {}
+  emoticons.each do |name,arrays|
+      frank_emoticons[name] ||= {}
+      frank_emoticons[name][:english] = arrays[0]
+      frank_emoticons[name][:japanese] = arrays[1]
   end
-frank_emo
+frank_emoticons
+end
+
+
+def get_japanese_emoticon(path, emoticon)
+  frank_emoticons = load_library(path)
+  result = frank_emoticons["get_emoticon"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found" 
+  end
+  result
+end
+
+def get_english_meaning(path, emoticon)
+  frank_emoticons = load_library(path)
+  result = frank_emoticons["get_meaning"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found" 
+  end
+  result
 end
 
