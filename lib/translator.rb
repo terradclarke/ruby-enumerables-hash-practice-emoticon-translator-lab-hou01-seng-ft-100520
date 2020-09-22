e5f59a8c163743328652aaca2236c5eb1ee0d5ee
Terranie Sims <terranie67@gmail.com>
2020-09-22 18:48:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,36 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+
+def load_library(emoticon_file)
+  emoticons = YAML.load_file('./lib/emoticons.yml')
+
+  emoticon_lib = {
+                  'get_meaning'  => { :english => {}},
+                  'get_emoticon' => { :japanese => {}} 
+    
+  }
+
+  emoticons.each do |meaning, value|  
+    # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
+    english = value[0]
+    japanese = value[1]
+    emoticon_lib['get_meaning'][japanese] = meaning
+    emoticon_lib['get_emoticon'][english] = japanese
+  end
+  emoticon_lib
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
 
-def get_english_meaning
-  # code goes here
-end
+def get_japanese_emoticon(emoticon_file, emoticon)
+  emoticon_lib = load_library(emoticon_file)
+  japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
+  japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
+end	
+
+
+def get_english_meaning(emoticon_file, emoticon)
+  # code goes here	  emoticon_lib = load_library(emoticon_file)
+  english_meaning = emoticon_lib['get_meaning'][emoticon]
+  english_meaning ? english_meaning : 'Sorry, that emoticon was not found'
+end	
